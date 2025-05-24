38b5725f47a8692235544212ebffaf02ddb7b31b
Azvy <Azvy@greencsv.dev>
2025-05-24 11:27:54 -0500

    Fix Header Img for blogs

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'GREEN|CSV[.1]';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Just thoughts on code, systems, and discussion.';
+
